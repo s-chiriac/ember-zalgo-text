e150a6277967096f52c4ee118ef18c264d191542
Sorin Chiriac <s.chiriac@live.com>
2018-04-15 02:15:26 +0100

    Initial commit

---
+++ addon/components/zalgo-text.js
@@ -0,0 +1,32 @@
+import Component from '@ember/component';
+import layout from '../templates/components/zalgo-text';
+
+export default Component.extend({
+  layout,
+  classNames: ['ember-zalgo-text'],
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    const lowerCombiningCharacterBoundary = 768;
+    const upperCombiningCharacterBoundary = 879;
+
+    let outputString = '';
+
+    this.get('string').split('').forEach((character) => {
+      outputString += character;
+
+      if (character === ' ') {
+        return;
+      }
+
+      let numberOfCombiningCharacters = Math.floor(Math.random() * 10) + 10;
+
+      for (let i = 0; i < numberOfCombiningCharacters; i++) {
+        outputString += Math.floor(Math.random() * (upperCombiningCharacterBoundary - lowerCombiningCharacterBoundary)) + lowerCombiningCharacterBoundary;
+      }
+    });
+
+    this.set('outputString', outputString);
+  },
+});
---
+++ app/components/zalgo-text.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-zalgo-text/components/zalgo-text';
